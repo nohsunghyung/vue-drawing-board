921d86412ff56a073b81b28e856b9ec71409e5e1
Nohsunghyung <nolu8085@naver.com>
2020-04-10 18:52:44 +0900

    feat : 그림판 기능 추가

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,178 @@
 <template>
-	<div id="app"></div>
+	<div id="app">
+		<div id="content">
+			<canvas
+				id="canvas"
+				ref="canvasEl"
+				@mousemove="onMouseMoveFn"
+				@mousedown="onMouseDownFn"
+				@mouseup="onMouseUpFn"
+			></canvas>
+			<div class="handle-container">
+				<div class="brush">
+					<p>브러쉬 조절</p>
+					<input
+						type="range"
+						class="js-range"
+						min="1"
+						max="10"
+						step="0.5"
+						value="2.5"
+						@change="onChangeBrush"
+					/>
+				</div>
+				<div class="btn-group">
+					<button type="button" class="btn btn-fill">Fill</button>
+					<a href="javascript:void(0);" class="btn btn-download">Download</a>
+					<button type="button" class="btn btn-clear">Reset</button>
+				</div>
+				<ul class="color-list" @click="colorChange">
+					<li class="color" style="background-color: #000000;">
+						<i class="icon"></i>
+					</li>
+					<li class="color" style="background-color: #ff0000;">
+						<i class="icon"></i>
+					</li>
+					<li class="color" style="background-color: #000cff;">
+						<i class="icon"></i>
+					</li>
+					<li class="color" style="background-color: #00ff2a;">
+						<i class="icon"></i>
+					</li>
+					<li class="color" style="background-color: #fff000;">
+						<i class="icon"></i>
+					</li>
+					<li class="color" style="background-color: #ff8400;">
+						<i class="icon"></i>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	data() {
+		return {
+			ctx: null,
+			isDrawing: false,
+			canvasStyle: {
+				width: 700,
+				height: 700,
+				color: '#222',
+			},
+			colors: [{}],
+		};
+	},
+	methods: {
+		canvasInit(el) {
+			el.strokeStyle = this.canvasStyle.color;
+			el.fillStyle = this.canvasStyle.color;
+			el.lineWidth = 2;
+		},
+		onMouseMoveFn(event) {
+			const x = event.offsetX;
+			const y = event.offsetY;
+			if (!this.isDrawing) {
+				this.ctx.beginPath();
+				this.ctx.moveTo(x, y);
+			} else {
+				this.ctx.lineTo(x, y);
+				this.ctx.stroke();
+			}
+		},
+		onMouseDownFn() {
+			this.isDrawing = true;
+		},
+		onMouseUpFn() {
+			this.isDrawing = false;
+		},
+		onChangeBrush(e) {
+			const value = e.target.value;
+			this.ctx.lineWidth = value;
+		},
+		colorChange(e) {
+			console.dir(e.target);
+		},
+	},
+	mounted() {
+		// 캔버스 초기화
+		const canvas = this.$refs.canvasEl;
+		const ctx = canvas.getContext('2d');
+		this.ctx = ctx;
+		canvas.width = this.canvasStyle.width;
+		canvas.height = this.canvasStyle.height;
+		this.canvasInit(this.ctx);
+	},
+};
 </script>
 
-<style></style>
+<style lang="scss">
+@import './assets/css/reset.css';
+#canvas {
+	display: block;
+	width: 700px;
+	height: 700px;
+	margin: 40px auto 0;
+	border: 1px solid #000;
+	background-color: #fff;
+}
+.handle-container {
+	width: 700px;
+	margin: 40px auto;
+	text-align: center;
+}
+.color-list {
+	display: flex;
+	justify-content: center;
+	width: 700px;
+	margin: 0 auto;
+	li {
+		position: relative;
+		width: 30px;
+		height: 30px;
+		border-radius: 50%;
+		cursor: pointer;
+		+ li {
+			margin-left: 10px;
+		}
+		.icon {
+			display: none;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			width: 8px;
+			height: 8px;
+			margin-left: -4px;
+			margin-top: -4px;
+			background-color: #fff;
+			border-radius: 50%;
+			border: 1px solid #ddd;
+		}
+		&.active {
+			.icon {
+				display: block;
+			}
+		}
+	}
+}
+.btn-group {
+	margin: 20px;
+	.btn {
+		display: inline-block;
+		padding: 10px 20px;
+		border: 1px solid #222;
+		font-size: 15px;
+		color: #222;
+		background-color: #fff;
+		cursor: pointer;
+	}
+	.btn-fill {
+		&.active {
+			color: #fff;
+			background-color: #000;
+		}
+	}
+}
+</style>
